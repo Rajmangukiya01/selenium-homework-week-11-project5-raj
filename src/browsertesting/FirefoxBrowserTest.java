7795fb4c8632094d85d1987eeb669dfba6772290
Rajmangukiya01 <rajmangukiya1002gmail.com>
2022-11-15 12:24:27 +0000

    My homework week 11 project 5

---
+++ src/browsertesting/FirefoxBrowserTest.java
@@ -0,0 +1,45 @@
+package browsertesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class FirefoxBrowserTest {
+    public static void main(String[] args) {
+        String baseUrl = "https://courses.ultimateqa.com/users/sign_in";
+        System.setProperty("webdriver.gheko.driver", "drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        //Launch URL
+        driver.get(baseUrl);
+
+        //Maximize window
+        driver.manage().window().maximize();
+
+        //We give implicit time to driver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //Print the title of the page
+        System.out.println("Page title is : " + driver.getTitle());
+
+        //print the current url
+        System.out.println("Current url is : " + driver.getCurrentUrl());
+
+        //print the page source
+        System.out.println("Page source is : " + driver.getPageSource());
+
+        //Enter email in email field
+        WebElement emailField = driver.findElement(By.id("user[email]"));
+        emailField.sendKeys("prime123@gmail.com");
+
+        //Enter password in password field
+        WebElement passwordField = driver.findElement(By.name("user[password]"));
+        passwordField.sendKeys("prime123");
+
+        //close driver
+        driver.close();
+    }
+}
